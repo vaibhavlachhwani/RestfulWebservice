b56036c37d77e15d8516cd95acdd4e3eabe97847
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-12-25 21:54:25 +0530

    implement POST Rest endpoint for User

--- src/main/java/com/vaibhav/restfulwebservice/user/UserDAOService.java
+++ src/main/java/com/vaibhav/restfulwebservice/user/UserDAOService.java
@@ -8,14 +8,16 @@ import java.util.List;
 
 @Component
 public class UserDAOService {
+
     private static List<User> users = new ArrayList<>();
+    private static int userCount = 100;
 
     static {
-        users.add(new User(100, "jane", LocalDate.of(2000, 10, 25)));
-        users.add(new User(101, "jonah", LocalDate.of(2004, 10, 30)));
-        users.add(new User(102, "dina", LocalDate.of(1998, 12, 25)));
-        users.add(new User(103, "amy", LocalDate.of(1999, 1, 25)));
-        users.add(new User(104, "chey", LocalDate.of(2004, 4, 28)));
+        users.add(new User(++userCount, "jane", LocalDate.of(2000, 10, 25)));
+        users.add(new User(++userCount, "jonah", LocalDate.of(2004, 10, 30)));
+        users.add(new User(++userCount, "dina", LocalDate.of(1998, 12, 25)));
+        users.add(new User(++userCount, "amy", LocalDate.of(1999, 1, 25)));
+        users.add(new User(++userCount, "chey", LocalDate.of(2004, 4, 28)));
     }
 
     public List<User> findAll() {
@@ -23,6 +25,7 @@ public class UserDAOService {
     }
 
     public User save(User user) {
+        user.setId(++userCount);
         users.add(user);
         return user;
     }
