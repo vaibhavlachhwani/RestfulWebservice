342519ed19ad6d12df4f080baccfb634bffbb082
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-12-25 20:25:59 +0530

    use path variables

--- src/main/java/com/vaibhav/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/vaibhav/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.vaibhav.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(path = "/hello-world")
+    @GetMapping(path = "/hello-world")  
     public String helloWorld() {
         return "Hello, World!";
     }
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(value = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello, World!");
+    }
+
+    @GetMapping(value = "/hello-world/path-var/{name}")
+    public HelloWorldBean helloWorldBeanPathVar(@PathVariable String name) {
+        return new HelloWorldBean("Hello, " + name);
     }
 }
