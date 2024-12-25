51216659aa84da4552ab3de1d8bd718f8205a9f9
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-12-25 14:03:25 +0530

    create simplest hello world rest api

---
+++ src/main/java/com/vaibhav/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.vaibhav.restfulwebservice.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
