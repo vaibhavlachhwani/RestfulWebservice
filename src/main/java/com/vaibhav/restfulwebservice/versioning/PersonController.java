5128ef08f70db5f6ed71c18915505504ea79fafe
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-12-29 22:24:04 +0530

    add request param versioning for api

--- src/main/java/com/vaibhav/restfulwebservice/versioning/PersonController.java
+++ src/main/java/com/vaibhav/restfulwebservice/versioning/PersonController.java
@@ -2,6 +2,7 @@ package com.vaibhav.restfulwebservice.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,16 @@ public class PersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionPersonReqParam() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionPersonReqParam() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
