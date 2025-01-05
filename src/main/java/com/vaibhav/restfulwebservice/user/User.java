23ba0c9a6eee168bb2993755caf6d5784d7b898b
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-01-06 01:56:34 +0530

    switch to h2 db

--- src/main/java/com/vaibhav/restfulwebservice/user/User.java
+++ src/main/java/com/vaibhav/restfulwebservice/user/User.java
@@ -1,23 +1,29 @@
 package com.vaibhav.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotBlank(message = "username is required")
-    @JsonProperty("name")
+//    @JsonProperty("name")
     private String username;
 
     @NotNull(message = "dateOfBirth is required")
     @PastOrPresent(message = "dateOfBirth is invalid")
-    @JsonProperty("dob")
+//    @JsonProperty("dob")
     private LocalDate dateOfBirth;
 
     public User(int id, String username, LocalDate dateOfBirth) {
--- src/main/java/com/vaibhav/restfulwebservice/user/UserDAOService.java
+++ src/main/java/com/vaibhav/restfulwebservice/user/UserDAOService.java
@@ -1,45 +1,46 @@
 package com.vaibhav.restfulwebservice.user;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class UserDAOService {
 
-    private static final List<User> users = new ArrayList<>();
-    private static int userCount = 100;
+//    private static final List<User> users = new ArrayList<>();
+//    private static int userCount = 100;
+//
+//    static {
+//        users.add(new User(++userCount, "jane", LocalDate.of(2000, 10, 25)));
+//        users.add(new User(++userCount, "jonah", LocalDate.of(2004, 10, 30)));
+//        users.add(new User(++userCount, "dina", LocalDate.of(1998, 12, 25)));
+//        users.add(new User(++userCount, "amy", LocalDate.of(1999, 1, 25)));
+//        users.add(new User(++userCount, "jake", LocalDate.of(2004, 4, 28)));
+//    }
 
-    static {
-        users.add(new User(++userCount, "jane", LocalDate.of(2000, 10, 25)));
-        users.add(new User(++userCount, "jonah", LocalDate.of(2004, 10, 30)));
-        users.add(new User(++userCount, "dina", LocalDate.of(1998, 12, 25)));
-        users.add(new User(++userCount, "amy", LocalDate.of(1999, 1, 25)));
-        users.add(new User(++userCount, "jake", LocalDate.of(2004, 4, 28)));
+    private final UserRepository userRepository;
+
+    public UserDAOService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public List<User> findAll() {
-        return users;
+        return userRepository.findAll();
     }
 
     public User save(User user) {
-        user.setId(++userCount);
-        users.add(user);
-        return user;
+        return userRepository.save(user);
     }
 
     public User findOne(int id) {
-        return users.stream()
-                .filter(u -> u.getId() == id)
-                .findFirst()
+        return userRepository.findById(id)
                 .orElse(null);
     }
 
     public void deleteUserById(int id) {
-        users.removeIf(
-                user -> user.getId() == id
-        );
+        userRepository.deleteById(id);
     }
 }
